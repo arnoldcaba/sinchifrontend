362469c0d0f93d42e45c7747cc45fec73f598a79
Arnold Caballero <arnoldcaba@gmail.com>
2022-06-13 09:02:22 -0500

    intento 4

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/sinchifrontend/precache-manifest.024ed232b58be637ca2df82f0418e9ee.js"
+  "/precache-manifest.cd33dc8ec5157e5cc1185e80a8248f12.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "SINCHI"});
