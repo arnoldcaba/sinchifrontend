08d09bfff694ae6c50b81a3534e1e5ae17fe0e29
Arnold Caballero <arnoldcaba@gmail.com>
2022-06-13 08:46:53 -0500

    creating changing the path

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8f4373e1c49a797c094740c9762ed0dc.js"
+  "/sinchifrontend/precache-manifest.024ed232b58be637ca2df82f0418e9ee.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "SINCHI"});
