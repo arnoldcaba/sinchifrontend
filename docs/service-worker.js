1e145c378e7c5d54333860ac16f487cd1f1c2f88
Arnold Caballero <arnoldcaba@gmail.com>
2022-06-13 08:19:38 -0500

    creating docs folder

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/SinchiEnd/precache-manifest.37165344af092b6a30a3554a2f3f05ee.js"
+  "/precache-manifest.8f4373e1c49a797c094740c9762ed0dc.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "SINCHI"});
