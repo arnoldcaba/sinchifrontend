1317c103e323089d7e6aba652c42da827a58a3be
Arnold Caballero <arnoldcaba@gmail.com>
2022-07-04 13:12:04 -0500

    Ajustes para revision de PQRSDF

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.576d257eb1d1be90b4b96fac6cf71102.js"
+  "/precache-manifest.71acf95391dcd394088071aa5ae9ff10.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "SINCHI"});
